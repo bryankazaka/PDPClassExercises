f0f3703b9427fc0facd907e8c039284c5ba1591d
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2024-11-18 12:12:07 -0500

    bomb

--- StringDecorator.java
+++ StringDecorator.java
@@ -1,9 +1,29 @@
-public class StringDecorator {
-    
-    private String inputString; 
+import java.io.PrintWriter;
 
-    public StringDecorator(String inputString)
-    {
+public class StringDecorator {
+
+    private String inputString;
+
+    public StringDecorator(String inputString) {
         this.inputString = inputString;
     }
+
+
+    public void jsonDecorator() {
+        PrintWriter pw = new PrintWriter(inputString + ".json", "UTF-8");
+        String[] splitByNewline = inputString.split(",");
+        pw.println("[");
+        pw.println("    {");
+        for (int i = 0; i < splitByNewline.length; i++) {
+            pw.print("      "+splitByNewline[i]);
+            pw.print(":");
+            pw.print("header1");
+            pw.println(",");
+            pw.println("    },");
+            pw.println();
+        }
+
+        pw.println("]");
+    }
+}
 }
