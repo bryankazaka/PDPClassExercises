d8d5219b50f96b7f60ab3772bf0ffceb0433ff81
Winner Kazaka <117994759+bryankazaka@users.noreply.github.com>
2024-11-18 11:49:07 -0500

    new repo

--- Main.java
+++ Main.java
@@ -1,24 +1,10 @@
-import src.BST;
-
-import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-
-        BST bst = new BST();
-        Random random = new Random();
-
-        for (int i = 0; i < 10; i++) {
-            int randomNumber = random.nextInt(100) + 1;
-            bst.put(randomNumber);
-        }
-
-        System.out.println("Inorder");
-        bst.inorder();
-        System.out.println("\nPostorder");
-        bst.postorder();
-        System.out.println("\nPreorder");
-        bst.preorder();
-
+        Scanner scanner = new Scanner(System.in);
+        String sysin = scanner.nextLine();
+        
     }
 }
